3616dc5ed06be1505946490a5f3d95094666f1f4
alieldeba <alieldeba375@gmail.com>
2024-03-22 17:29:27 +0200

    CU-86by0x1fz - Apply dark mode with shadcn

--- components/Features.tsx
+++ components/Features.tsx
@@ -126,10 +126,10 @@ function Features() {
 
   return (
     <section className="py-14">
-      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8">
         <div className="relative max-w-2xl mx-auto sm:text-center">
           <div className="relative z-10">
-            <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+            <h3 className="text-3xl font-semibold sm:text-4xl">
               Let’s help power your SaaS
             </h3>
             <p className="mt-3">
@@ -150,12 +150,10 @@ function Features() {
             {features.map((item, idx) => (
               <li
                 key={idx}
-                className="bg-white space-y-3 p-4 border rounded-lg"
+                className="space-y-3 p-4 border rounded-lg"
               >
                 <div className="text-indigo-600 pb-3">{item.icon}</div>
-                <h4 className="text-lg text-gray-800 font-semibold">
-                  {item.title}
-                </h4>
+                <h4 className="text-lg font-semibold">{item.title}</h4>
                 <p>{item.desc}</p>
               </li>
             ))}
