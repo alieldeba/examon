3ccf8b78f1c2bff5071f0d73715e64bb9eeb59c5
alieldeba <alieldeba375@gmail.com>
2024-03-26 02:10:11 +0200

    fix: secondary 3D dark button active effect

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-[#e8ebee] dark:hover:bg-[#1c2737] shadow-[0_4px_0_#d1d5d9] dark:shadow-[0_4px_0_#1e293bb8] active:shadow-none active:translate-x-0 active:translate-y-1",
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-[#e8ebee] dark:hover:bg-[#1c2737] shadow-[0_4px_0_#d1d5d9] dark:shadow-[0_4px_0_#1e293bb8] active:shadow-none dark:active:shadow-none active:translate-x-0 active:translate-y-1",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
